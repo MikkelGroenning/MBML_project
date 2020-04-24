7ba6cf500e10df7a5c2817c8ff8cae6fc43138f9
Mikkel Grønning <5516569-MikkelGroenning@users.noreply.gitlab.com>
2020-04-24 12:57:35 +0200

    don't fuck up plz

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,55 +1,64 @@
-#%%
-import xml.etree.ElementTree as ET
 import pandas as pd
 import os
-import sys
+import sys 
+import xml.etree.ElementTree as ET
+from pathlib import Path
 
-file_path = os.path.dirname(__file__)
+data_path = Path(__file__).parent / "../../data/interim/data.pickle"
 
-
-def tale_iterator(et):
+def tale_iterator( et, verbose = False ):
     current_meeting_id = None
+    missing_observations = 0
+    found_observations = 0
+
     for element in et.iter():
-        if element.tag == "Tale":
-            observation = {x.tag: x.text for x in element}
-            observation.update({"MeetingId": current_meeting_id})
-            yield observation
-        elif element.tag == "Møde":
+        if element.tag == 'Tale':
+            observation = {x.tag : x.text for x in element}
+            observation.update( { 'MeetingId' : current_meeting_id } )
+
+            if not 'Tekst' in  observation or observation['Tekst'] is None:
+                missing_observations += 1
+            else:
+                found_observations += 1
+                yield observation
+
+        elif element.tag == 'Møde':
             try:
-                current_meeting_id = next(
-                    (x for x in element if x.tag == "MeetingId")
-                ).text
+                current_meeting_id = next( (x for x in element if x.tag == 'MeetingId') ).text 
             except StopIteration:
-                raise Warning("No meeting category found")
+                raise Warning('No meeting category found')
 
+    if verbose:
+        print('Successfully read %d speeches, found %d to be missing.' % \
+            (found_observations, missing_observations))
 
-# If file is directly run by python:
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     df = pd.DataFrame()
-
-    # Reading data from each file, appending each 'Tale' entry to dataframe
-    for file in os.scandir(file_path + "/../../data/raw/"):
-
-        if file.path.endswith(".xml"):
-            print("Proccessing: {0}".format(file.name))
+    
+    #%% Reading data from each file, appending each 'Tale' entry to dataframe
+    for file in os.scandir( Path(__file__).parent / '../../data/raw/', ):
+        if file.path.endswith('.xml'):
+            print(f"Processing file: {file.name}")
             et = ET.parse(file)
-            df = df.append(list(tale_iterator(et)))
+            df = df.append( list( tale_iterator(et, verbose=True) ) )
+    
+    #%% Proper datatypes, sorting
 
-    # Proper datatypes, and sorting
-    df["Starttid"] = pd.to_datetime(df["Starttid"])
-    df["Sluttid"] = pd.to_datetime(df["Sluttid"])
+    print('Sorting data according to time...', end=' ')
+    df['Starttid'] = pd.to_datetime(df['Starttid'])
+    df['Sluttid'] = pd.to_datetime(df['Sluttid'])
+    print('Done!')
 
-    df.sort_values("Starttid", inplace=True)
+    df.sort_values('Starttid', inplace=True)
     df.reset_index(inplace=True, drop=True)
 
     #%% Pickling
-    print("Saving to pickle...", end=" ")
-    df.to_pickle(file_path + "/../../data/interim/data.pickle")
-    print("Pickle Created")
+    print("Saving to pickle...", end=' ')
+    df.to_pickle( data_path )
+    print("Done!")
 
-# if the file is imported:
 else:
-    try:
-        df = pd.read_pickle(file_path + "/../../data/interim/data.pickle")
-    except FileNotFoundError:
-        print("Pickle not created yet - run make_dataset.py")
+
+    df = pd.read_pickle( data_path )
+    
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,12 +1,30 @@
-
 import pandas as pd
 import os
 import sys
 import numpy as np
-from func import *
+import nltk
+import requests
+import time
+from pathlib import Path
 
-processed_path = os.path.dirname(__file__) + "/../../data/processed/data.pickle"
-interim_path = os.path.dirname(__file__) + "/../../data/interim/data.pickle"
+import pickle
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+sys.path.append(str(Path(__file__).parent.resolve() / ".."))
+from data.make_dataset import df as interim_df
+
+processed_path = Path(__file__).parent.resolve() / ".." / ".." / "data" / "processed"
+
+# From https://stackoverflow.com/questions/36182502/add-stemming-support-to-countvectorizer-sklearn
+danish_stemmer = nltk.stem.SnowballStemmer("danish")
+
+
+class StemmedCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedCountVectorizer, self).build_analyzer()
+        return lambda doc: ([danish_stemmer.stem(w) for w in analyzer(doc)])
+
 
 # Consider JSON
 name_party_dict = {
@@ -117,13 +135,71 @@ name_party_dict = {
 }
 
 
-if __name__ == "__main__":
-    
-    interim_df = pd.read_pickle(interim_path)
-    df = interim_df.copy()
+def stem(word_array):
+    stemmer = nltk.stem.SnowballStemmer("danish")
 
-    # Building features
-    print("Building features...")
+    try:
+        return [stemmer.stem(word) for word in word_array]
+    except TypeError:
+        return []
+
+
+def read_txt_file(path_to_file):
+
+    f = open(path_to_file, "r")
+    words = f.read().splitlines()
+    f.close()
+
+    return words
+
+
+def get_stop_words():
+
+    path = (
+        Path(__file__).parent.resolve()
+        / ".."
+        / ".."
+        / "data"
+        / "external"
+        / "stopord.txt"
+    )
+    try:
+        stopwords = read_txt_file(path)
+    except FileNotFoundError:
+
+        print("Downloading stopwords from Github")
+        url = "https://gist.githubusercontent.com/berteltorp/0cf8a0c7afea7f25ed754f24cfc2467b/raw/305d8e3930cc419e909d49d4b489c9773f75b2d6/stopord.txt"
+        r = requests.get(url)
+        with open(path, "wb") as f:
+            f.write(r.content)
+        stopwords = read_txt_file(path)
+    return stopwords
+
+
+def remove_stop_words(words, stopwords):
+
+    try:
+        return [x for x in words if x not in stopwords]
+    except TypeError:
+        return words
+
+
+if __name__ == "__main__":
+
+    t = time.time()
+
+    df = interim_df
+
+    # Fjern speaker
+    print("Removing speakers...")
+    tmp = sum(df.Rolle.str.contains("formand"))
+    df = df.drop(df.loc[df["Rolle"].str.contains("formand")].index)
+    df.reset_index(inplace=True, drop=True)
+
+    print(f"Removed {tmp} observations...")
+
+    #%% Adding additional features
+    print("Adding additional data...")
 
     # Filling party in missing observation
     df.Parti = df.Parti.fillna(df.Navn.map(name_party_dict))
@@ -150,26 +226,38 @@ if __name__ == "__main__":
     choices = ["0Lars1", "Helle1", "Helle2", "Lars2", "Lars3"]
     df["Regering"] = np.select(conditions, choices, default=None)
 
-    # Splitting tekst into lists
-    df["Tekst"] = df["Tekst"].str.split("[^A-Za-zåøæØÅÆ1-9]+")
+    # Er parti i regering
+    govs = [["V", "KF"], ["S", "RV", "SF"], ["S", "RV"], ["V"], ["V", "KF", "LA"]]
+    government_dict = dict(zip(choices, govs))
+    df["I_Regering"] = [
+        i in j for i, j in zip(df.Parti, df["Regering"].map(government_dict))
+    ]
 
-    # Stop-words
-    print("Removing stop word (expect long time) ... ", end=" ")
-    df["Tekst"] = df["Tekst"].apply(remove_words, stopwords=get_stop_words())
-    print("Removing done!")
-    #%%
-    # Stemming
-    print("Steming word (expect long time) ... ", end=" ")
-    df["Tekst"] = df["Tekst"].apply(stem)
-    print("Steming done!")
+    # Merger Nordatlantiske partier
+    NApartier = ["IA", "SIU", "JF", "T", "SP", "TF"]
+    df = df.replace(to_replace=NApartier, value="NA")
 
-    #%% Pickling processed_path data
-    print("Saving to pickle...", end=" ")
-    df.to_pickle(processed_path)
-    print("Done!")
+    # Vectorizing speeches
+    print("Vectorizing speeches...")
+    vectorizer = StemmedCountVectorizer(
+        min_df=10, analyzer="word", stop_words=get_stop_words()
+    )
+    X = vectorizer.fit_transform(df["Tekst"])
+    vocabulary = vectorizer.vocabulary_
+    tfidf = TfidfTransformer()
+    X_tfidf = tfidf.fit_transform(X)
+
+    print("Saving dataframe to pickle...")
+    df.to_pickle(processed_path / "data.pickle")
+
+    print("Saving vectorized data to pickle...")
+    with open(processed_path / "vectorized.pickle", "wb") as f:
+        pickle.dump((vocabulary, X, X_tfidf), f)
+
+    print("Done! (Took %.2f seconds)" % (time.time() - t))
 
 else:
+    df = pd.read_pickle(processed_path / "data.pickle")
 
-    df = pd.read_pickle(processed_path)
-
-
+    with open(processed_path / "vectorized.pickle", "rb") as f:
+        vocabulary, X, X_tfidf = pickle.load(f)
